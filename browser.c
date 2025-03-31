6d30fb34e0c506f04701ef2bcb24d3c853c5b799
marasescu-stefan <stefan.marasescu@stud.acs.upb.ro>
2025-04-01 00:55:36 +0300

    aproape gata structurile

--- browser.c
+++ browser.c
@@ -5,9 +5,37 @@
 
 #include <string.h>
 
+struct page {
+	int id;
+	char url[50];
+	char *description;
+}
+
+struct stack {
+	unsigned int size;
+	//de completat
+}
+
+struct tab {
+	int id;
+	struct page *currentPage;
+	struct stack *backwardStack;
+	struct stack *forwardStack;
+}
+
+struct tabList {
+	unsigned int size;
+	//de completat
+}
+
+struct browser {
+	struct tab *current;
+	struct tabList list;
+}
+
 int main(void)
 {
-	
+
 
 	return 0;
 }
